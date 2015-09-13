64a2bc4f999820fc9ec4cedd689ff674db431cde
OnlyD <islasvictorm@gmail.com>
2015-09-13 18:15:10 -0500

    modified:   app/AppKernel.php 	modified:   app/config/config.yml 	modified:   app/config/routing.yml

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,23 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Agenda_ILM\ContactoBundle\ContactoBundle(),
+            new Agenda_ILM\ClienteBundle\ClienteBundle(),
+            new Agenda_ILM\ProveedorBundle\ProveedorBundle(),
+            new Agenda_ILM\CorreoBundle\CorreoBundle(),
+            new Agenda_ILM\TelefonoBundle\TelefonoBundle(),
+            new Agenda_ILM\DireccionBundle\DireccionBundle(),
+            new Agenda_ILM\TipoDatoBundle\TipoDatoBundle(),
+            new Agenda_ILM\CityDatoBundle\CityDatoBundle(),
+            new Agenda_ILM\CityBundle\CityBundle(),
+            new Agenda_ILM\CountryBundle\CountryBundle(),
+            new Agenda_ILM\LanguageBundle\LanguageBundle(),
+            new Agenda_ILM\EventoBundle\EventoBundle(),
+            new Agenda_ILM\SeguimientoBundle\SeguimientoBundle(),
+            new Agenda_ILM\RecordatorioBundle\RecordatorioBundle(),
+            new Agenda_ILM\ProductoBundle\ProductoBundle(),
+            new Agenda_ILM\DetalleProductoBundle\DetalleProductoBundle(),
+            new Agenda_ILM\UsuarioBundle\UsuarioBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
