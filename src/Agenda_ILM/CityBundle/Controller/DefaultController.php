16304ab6a295a00923cdfc82a7c88087f3ce5032
kst17 <cast.beto@gmail.com>
2015-09-20 14:49:05 -0500

    modified:   src/Agenda_ILM/CityBundle/Controller/DefaultController.php

--- src/Agenda_ILM/CityBundle/Controller/DefaultController.php
+++ src/Agenda_ILM/CityBundle/Controller/DefaultController.php
@@ -4,10 +4,8 @@ namespace Agenda_ILM\CityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
+class DefaultController extends Controller{
+    public function indexAction($name){
         return $this->render('CityBundle:Default:index.html.twig', array('name' => $name));
     }
 }
