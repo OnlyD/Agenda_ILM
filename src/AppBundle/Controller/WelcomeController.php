a45bc1c021eebc939b7fde1399df51d27cdadd3f
analuciavazquez <dashurilu@hotmail.com>
2015-11-08 01:03:22 -0600

    modified:   app/Resources/views/base.html.twig 	new file:   app/Resources/views/default/alert.html.twig 	new file:   app/Resources/views/default/datosusuario.html.twig 	new file:   app/Resources/views/default/home.html.twig 	renamed:    app/Resources/views/base.html.twig -> app/Resources/views/layoutcomplet.html.twig 	modified:   app/config/routing_dev.yml 	new file:   src/AppBundle/Controller/WelcomeController.php 	new file:   web/dist/img/logmin.png

---
+++ src/AppBundle/Controller/WelcomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class WelcomeController extends Controller
+{
+    /**
+     * @Route("/app/example", name="homepage")
+     */
+    public function homeAction()
+    {
+        return $this->render('default/home.html.twig');
+    }
+	public function alertAction()
+    {
+        return $this->render('default/alert.html.twig');
+    }
+	public function nameuserAction()
+    {
+		
+        return $this->render('default/datosusuario.html.twig');
+    }
+}
